879906c5538a3e5977eb7c387941d83c77e7e6c2
Luke Dornburgh <dornbule@dukes.jmu.edu>
2022-10-23 15:37:51 -0400

    adding invite to project functionality

--- Pages/ProjectsPages/ProjectInvite.cshtml.cs
+++ Pages/ProjectsPages/ProjectInvite.cshtml.cs
@@ -1,12 +1,42 @@
+using Lab1.Pages.DB_Class;
+using Lab1.Pages.DataClasses;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Identity;
 
 namespace Lab1.Pages.ProjectsPages
 {
     public class ProjectInviteModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public int selectedProject { get; set; }
+
+        public List<Projects> projectDropDown = new List<Projects>();
+        public void OnGet(int UserID)
         {
+            SqlDataReader myProjects = DBClass.MyProjectsTableReader(HttpContext.Session.GetString("username"));
+
+            while (myProjects.Read())
+            {
+                projectDropDown.Add(new Projects
+                {
+                    ProjectID = Int32.Parse(myProjects["ProjectID"].ToString()),
+                    ProjectName = myProjects["ProjectName"].ToString(),
+                    ProjectDescription = myProjects["ProjectDescription"].ToString(),
+                    ProjectType = myProjects["ProjectType"].ToString(),
+                    UserID = Int32.Parse(myProjects["UserID"].ToString()),
+                });
+            }
+            myProjects.Close();
+        }
+
+        public IActionResult OnPost()
+        {
+            //call a db class method to insert an invite into the invite bridge table
+
+
+            return RedirectToPage();
         }
     }
 }

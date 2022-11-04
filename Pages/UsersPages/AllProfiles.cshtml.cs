efa7c438844c82563852e6804089dfee79daaf9e
Luke Dornburgh <dornbule@dukes.jmu.edu>
2022-11-04 18:54:57 -0400

    a

--- Pages/UsersPages/AllProfiles.cshtml.cs
+++ Pages/UsersPages/AllProfiles.cshtml.cs
@@ -10,23 +10,22 @@ namespace Lab1.Pages.UsersPages
     {
         public List<Users> UserList { get; set; }
 
-        [BindProperty]
-        public string searchText { get; set; }
-
         public SqlDataReader returnReader { get; set; }
 
         [BindProperty]
         public List<int> SelectedSkills { get; set; }
+
+        public List<Skills> skillsAlreadySelected { get; set; }
 
         public List<Skills> SkillsDisplay { get; set; }
         public AllProfilesModel()
         {
             UserList = new List<Users>();
             SkillsDisplay = new List<Skills>();
+            skillsAlreadySelected = new List<Skills>();
         }
         public IActionResult OnGet()
         {
-
             int temp = DBClass.GetUserIDSession(HttpContext.Session.GetString("username"));
             int badgeNum = DBClass.NotificationNumber(temp);
             HttpContext.Session.SetInt32("badgeNum", badgeNum);
@@ -77,15 +76,52 @@ namespace Lab1.Pages.UsersPages
         }
         public IActionResult OnPost()
         {
+            //I need to create a new list equal to the value of the selected skills
+            //then I need to pass it into the redirect so it can be grabbed in OnGet()
+            //then, in the get we need to create skill objects for each of the selected skills
+            //then in the view, we iterate over the list of skill objects and display them as pills and make sure they begin checked
+
+            List<int> justSelected = new List<int>();
+
             string skillIDList = "";
 
             foreach (var num in SelectedSkills)
             {
+                justSelected.Add(num);
                 skillIDList += num + " ";
             }
             returnReader = DBClass.FilterUsersBySkill(skillIDList, HttpContext.Session.GetString("username"));
 
+            if (justSelected != null)
+            {
+                foreach (var num in justSelected)
+                {
+                    SqlDataReader preSkillReader = DBClass.GetSpecificSkillObject(num);
+                    while (preSkillReader.Read())
+                    {
+                        skillsAlreadySelected.Add(new Skills
+                        {
+                            SkillID = Int32.Parse(preSkillReader["SkillID"].ToString()),
+                            SkillName = preSkillReader["SkillName"].ToString()
+                        });
+                    }
+                }
+            }
+
+            SqlDataReader varSkillReader = DBClass.SkillsTableReader();
+            while (varSkillReader.Read())
+            {
+                SkillsDisplay.Add(new Skills
+                {
+                    SkillID = Int32.Parse(varSkillReader["SkillID"].ToString()),
+                    SkillName = varSkillReader["SkillName"].ToString(),
+                });
+            }
+
+            varSkillReader.Close();
+
             return Page();
+            //return RedirectToPage(new { alreadySelected = justSelected });
         }
     }
 
